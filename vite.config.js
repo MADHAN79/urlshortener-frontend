ad4f1ebb0f2b16d343fdbf4f4784b4a055291c2b
MADHAN79 <madhanvarshini3@gmail.com>
2024-09-09 10:39:43 +0530

    cdn added in vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import cdn from 'vite-plugin-cdn-import'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react", "react-dom", "react-router-dom"],
-    },
-  },
-})
+export default {
+    plugins: [
+        cdn({
+            modules: ['react', 'react-dom', 'react-router-dom'],
+        }),
+    ],
+}
