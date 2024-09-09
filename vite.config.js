5a01190c826435245ffd44a4c61b402869108446
MADHAN79 <madhanvarshini3@gmail.com>
2024-09-09 10:49:08 +0530

    back to rollupoptions

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-// vite.config.js
-import cdn from 'vite-plugin-cdn-import'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default {
-    plugins: [
-        cdn({
-            modules: ['react', 'react-dom', 'react-router-dom'],
-        }),
-    ],
-}
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["react", "react-dom", "react-router-dom"],
+    },
+  },
+})
